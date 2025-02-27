e83d1d7bd599966659088e8de7c9a633a85d4ee6
ben-a-gee <benjaminandrewgee@icloud.com>
2025-02-27 17:28:49 +0000

    Update for loop

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,15 +1,24 @@
-message = "Hello, World!" 
-shift = 17
+message = "Hello, World!"
+shift = 7
 
-# loop through each of the charictor in the message.
+result = ""
+
+# loop through each of the charactor in the message.
 for char in message:
-    # Convert charictor to the ASCII code 
-    char_code = ord(char)
-    
-    # Create a new charactor code by adding the shift to the current charactor code
-    new_char_code = char_code + shift
-    
-    # convert the new charactor code to a new charactor
-    new_char = chr(new_char_code)
+    # it checks if char is in the alphabet (a - z)
+    if char.isalpha():
+        # Convert charactor to the ASCII code 
+        char_code = ord(char)
+        
+        # Create a new charactor code by adding the shift to the current charactor code
+        new_char_code = char_code + shift
+        
+        # convert the new charactor code to a new charactor
+        new_char = chr(new_char_code)
+        
+        result = result + new_char
+    else:
+        # otherwise, if the char is not in the alphabet, 
+        result = result + char
 
-    print(char, char_code, new_char_code, new_char)
+print(result)
